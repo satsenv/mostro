2b139c40e14ec984a68c7c3d293a9b30b748704a
Jean-François Roche <jfroche@pyxel.be>
2026-03-20 17:40:24 +0100

    feat: enable lnd service via sats-dev modules with local settings generation

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   languages.rust = {
     enable = true;
@@ -7,10 +7,35 @@
 
   packages = [
     pkgs.protobuf
-    pkgs.htop
   ];
 
-  pre-commit.hooks = {
+  services.lnd = {
+    enable = true;
+    restPort = 7080;
+  };
+
+  env.MOSTRO_SETTINGS_DIR = "${config.devenv.state}/mostro";
+
+  enterShell = ''
+    mkdir -p "$MOSTRO_SETTINGS_DIR"
+    if [ ! -f "$MOSTRO_SETTINGS_DIR/settings.toml" ]; then
+      ${pkgs.gnused}/bin/sed \
+        -e "s|lnd_cert_file = .*|lnd_cert_file = '$LND_CERT_FILE'|" \
+        -e "s|lnd_macaroon_file = .*|lnd_macaroon_file = '$LND_MACAROON_FILE'|" \
+        -e "s|lnd_grpc_host = .*|lnd_grpc_host = '$LND_GRPC_HOST'|" \
+        settings.tpl.toml > "$MOSTRO_SETTINGS_DIR/settings.toml"
+      echo "Generated mostro settings at $MOSTRO_SETTINGS_DIR/settings.toml"
+      echo "Run mostrod with: cargo run -- -d $MOSTRO_SETTINGS_DIR"
+    fi
+  '';
+
+  git-hooks.tools = {
+    cargo = lib.mkForce config.languages.rust.toolchainPackage;
+    clippy = lib.mkForce config.languages.rust.toolchainPackage;
+    rustfmt = lib.mkForce config.languages.rust.toolchainPackage;
+  };
+
+  git-hooks.hooks = {
     clippy.enable = true;
     rustfmt.enable = true;
   };
